d198531d2cea7b2a7949aa010b3f1a9970f40b41
Teresa <hello@teresaromero.dev>
2021-10-12 09:42:14 +0200

    refactor: major refactor to add download scripts
    Signed-off-by: Teresa <hello@teresaromero.dev>

--- data/spotify/__main__.py
+++ data/spotify/__main__.py
@@ -1,57 +1,20 @@
 from constants import PLAYLISTS
-import client as spotify
 import pandas as pd
 import os
-
-
-def parse_track_info(tracks: list[dict], decade: str) -> list[dict]:
-    result = list()
-    for track in tracks:
-        info = dict()
-        info["track_id"] = track["id"]
-        info["uri"] = track["uri"]
-        info["popularity"] = track["popularity"]
-        info["title"] = track["name"]
-        info["duration_ms"] = track["duration_ms"]
-        info["artists"] = [a["name"] for a in track["artists"]]
-        info["release_date"] = track["album"]["release_date"]
-        info["release_date_precision"] = track["album"]["release_date_precision"]
-        info["preview_url"] = track["preview_url"]
-        info["decade"] = decade
-        result.append(info)
-    return result
-
-
-def parse_audio_analysis(tracks: list[dict]) -> list[dict]:
-    result = list()
-    for track in tracks:
-        analysis = spotify.audio_analysis(track["track_id"])
-        result.append({**track, **analysis})
-    return result
-
-
-def parse_audio_features(tracks: list[dict]) -> list[dict]:
-    result = list()
-    for track in tracks:
-        features = spotify.audio_features(track["track_id"])
-        result.append({**track, **features})
-    return result
-
-
-def get_playlist_tracks(playlist_id: str) -> list[dict]:
-    return spotify.playlist_tracks(playlist_id)
+import utils
 
 
 def main():
+    """deprecated - this used playlist to get the data"""
     df = pd.DataFrame()
     csv_path = os.path.join(os.path.abspath(os.getcwd()), "data/spotify/csv")
     os.makedirs(csv_path)
     for playlist in PLAYLISTS:
         print(f'Processing playlist {playlist["decade"]}')
-        p_tracks = get_playlist_tracks(playlist["playlist_id"])
-        p_tracks = parse_track_info(p_tracks, playlist["decade"])
-        p_tracks = parse_audio_analysis(p_tracks)
-        p_tracks = parse_audio_features(p_tracks)
+        p_tracks = utils.get_playlist_tracks(playlist["playlist_id"])
+        p_tracks = utils.parse_track_info(p_tracks, playlist["decade"])
+        p_tracks = utils.parse_audio_analysis(p_tracks)
+        p_tracks = utils.parse_audio_features(p_tracks)
         p_df = pd.DataFrame(p_tracks)
         p_df.to_csv(
             os.path.join(
@@ -69,26 +32,33 @@ def main():
 
 
 def search_main():
-    df = pd.DataFrame()
-    csv_path = os.path.join(os.path.abspath(
-        os.getcwd()), "data/spotify/csv/by_year")
-    os.makedirs(name=csv_path, exist_ok=True)
+    """main, this uses the search query by genre POP and year variable between 1900 and 2021"""
 
-    years = range(1900, 2022)
-    for year in years:
-        print("Year", year)
-        tracks = spotify.search(year)
-        if len(tracks) != 0:
-            p_df = pd.DataFrame(tracks)
-            p_df.to_csv(
-                os.path.join(
-                    csv_path, f'{year}_tracks.csv'),
-                index=False, index_label=False)
-            df = df.append(p_df)
-            df.to_csv(
-                os.path.join(
-                    csv_path, 'dataset.csv'),
-                index=False, index_label=False)
+    # create dataset from spotify api responses
+    csv_path_by_year = os.path.join(os.path.abspath(
+        os.getcwd()), "data/spotify/csv/by_year")
+    os.makedirs(name=csv_path_by_year, exist_ok=True)
+    csv_path = os.path.join(os.path.abspath(
+        os.getcwd()), "data/spotify/csv")
+    os.makedirs(name=csv_path, exist_ok=True)
+    period = range(1900, 2022)
+    df = utils.get_raw_dataset(
+        period, csv_path_by_year, csv_path)
+
+    # clean the dataset
+    df = utils.clean_dataset(df)
+
+    # save the clean df to csv
+    final_dataset_path = os.path.join(csv_path, "final_dataset.csv")
+    df.to_csv(final_dataset_path)
+
+    # download mp3 preview files
+    downloads_path = os.path.join(
+        os.path.abspath(os.getcwd()), "data/spotify/mp3")
+    os.makedirs(name=downloads_path, exist_ok=True)
+
+    utils.download_preview_urls(df, downloads_path)
+
     return
 
 
